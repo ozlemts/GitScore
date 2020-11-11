db4865c54f894c0d18225879741208082a8a2301
OzlemTS <ozlemturkerii@gmail.com>
2020-11-11 12:19:53 +0300

    added your own css class to tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="h-screen w-screen" style={ {background: 'linear-gradient(to bottom right, #fb786b, #ca476b)'} }>
       <Head>
-        <title>Create Next App</title>
+        <title>Git Score</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <div className="text-3xl font-bold">NEXT.JS DENEME</div>
+        <div className="text-3xl font-bold">Git Score</div>
         <div className="flex flex-row bg-gray-200">
             <a href="/about"><div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">ABOUT</div></a>
             <a href="/users"><div className="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">USERS</div></a>
+            <button className="btn btn-blue">okfjer</button>
         </div>
     </div>
   )
